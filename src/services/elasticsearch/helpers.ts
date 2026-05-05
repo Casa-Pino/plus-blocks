6a5b90e0bb604d12416dca4c6545a7ec4630915e
Rafael Oliveira <rafaelde0liveira@hotmail.com>
2026-05-05 10:22:00 -0300

    1.4.14

--- src/services/elasticsearch/helpers.ts
+++ src/services/elasticsearch/helpers.ts
@@ -1,17 +1,17 @@
 import { QueryStringParsed } from './shared';
 export function esHitsMap(data: any, mapper: Function): any[] {
-  return data?.hits?.hits?.map(mapper) || []
+  return data?.hits?.hits?.map(mapper) || [];
 }
 export function esTotalHits(data: any): number {
-  return data?.hits.total.value || 0
+  return data?.hits.total.value || 0;
 }
 export function countTotalPages(total: number, size: number): number {
-  return Math.ceil(total / size)
+  return Math.ceil(total / size);
 }
 export function getQueryStringPage(queryStrings: QueryStringParsed) {
   return Array.isArray(queryStrings?.page)
     ? Number.parseInt(queryStrings?.page[0])
     : queryStrings?.page
     ? Number.parseInt(queryStrings?.page)
-    : 1
+    : 1;
 }
--- src/services/elasticsearch/posts.ts
+++ src/services/elasticsearch/posts.ts
@@ -1,13 +1,8 @@
 import { esSearch } from './shared';
 import { QueryStringParsed } from './shared';
 import { esMapPostCard } from './mappers';
-import {
-  countTotalPages,
-  esHitsMap,
-  esTotalHits,
-  getQueryStringPage
-} from './helpers'
-const ES_INDEX = process.env.ELASTICSEARCH_NEWS_INDEX || 'post'
+import { countTotalPages, esHitsMap, esTotalHits, getQueryStringPage } from './helpers';
+const ES_INDEX = process.env.ELASTICSEARCH_NEWS_INDEX || 'post';
 export async function esQueryLastPosts(size = 3, from = 0) {
   const query = {
     from,
@@ -18,23 +13,20 @@ export async function esQueryLastPosts(size = 3, from = 0) {
       // filter: [
       //   { range: { createdAt: { gte: '2021-08-01' }}}
       // ]
-    }
-  }
-  return await esSearch(ES_INDEX, query)
+    },
+  };
+  return await esSearch(ES_INDEX, query);
 }
 export async function esGetLastPosts(limit = 3) {
-  const result = await esQueryLastPosts(limit)
-  return esHitsMap(result, esMapPostCard)
+  const result = await esQueryLastPosts(limit);
+  return esHitsMap(result, esMapPostCard);
 }
-export async function esGetLastPostsPaginate(
-  queryStrings: QueryStringParsed,
-  size = 10
-) {
-  const page = getQueryStringPage(queryStrings)
-  const from = size * page - size
-  const result = await esQueryLastPosts(size, from)
-  const total = esTotalHits(result)
-  const posts = esHitsMap(result, esMapPostCard)
+export async function esGetLastPostsPaginate(queryStrings: QueryStringParsed, size = 10) {
+  const page = getQueryStringPage(queryStrings);
+  const from = size * page - size;
+  const result = await esQueryLastPosts(size, from);
+  const total = esTotalHits(result);
+  const posts = esHitsMap(result, esMapPostCard);
   // const featured = []
   // if (news.length) featured.push({ ...news.shift(), highlight: true })
   // if (news.length >= 2) featured.push({ ...news.shift() })
@@ -46,15 +38,11 @@ export async function esGetLastPostsPaginate(
       page,
       size,
       total,
-      total_pages: countTotalPages(total, size)
-    }
-  }
+      total_pages: countTotalPages(total, size),
+    },
+  };
 }
-export async function esQueryCategoryPosts(
-  categorySlug: string,
-  size = 3,
-  from = 0
-) {
+export async function esQueryCategoryPosts(categorySlug: string, size = 3, from = 0) {
   const query = {
     from,
     size,
@@ -64,35 +52,31 @@ export async function esQueryCategoryPosts(
         path: 'categories',
         query: {
           bool: {
-            must: [{ match: { 'categories.slug.keyword': categorySlug } }]
-          }
-        }
-      }
-    }
-  }
-  return await esSearch(ES_INDEX, query)
+            must: [{ match: { 'categories.slug.keyword': categorySlug } }],
+          },
+        },
+      },
+    },
+  };
+  return await esSearch(ES_INDEX, query);
 }
 export async function esGetCategoryPosts(categorySlug: string, limit = 3) {
-  const result = await esQueryCategoryPosts(categorySlug, limit)
-  return esHitsMap(result, (hit: any) => esMapPostCard(hit, false))
+  const result = await esQueryCategoryPosts(categorySlug, limit);
+  return esHitsMap(result, (hit: any) => esMapPostCard(hit, false));
 }
-export async function esGetCategoryPostsPaginate(
-  categorySlug: string,
-  queryStrings: QueryStringParsed,
-  size = 10
-) {
-  const page = getQueryStringPage(queryStrings)
-  const from = size * page - size
-  const result = await esQueryCategoryPosts(categorySlug, size, from)
-  const total = esTotalHits(result)
-  const news = esHitsMap(result, esMapPostCard)
+export async function esGetCategoryPostsPaginate(categorySlug: string, queryStrings: QueryStringParsed, size = 10) {
+  const page = getQueryStringPage(queryStrings);
+  const from = size * page - size;
+  const result = await esQueryCategoryPosts(categorySlug, size, from);
+  const total = esTotalHits(result);
+  const news = esHitsMap(result, esMapPostCard);
   return {
     news,
     pagination: {
       page,
       size,
       total,
-      total_pages: countTotalPages(total, size)
-    }
-  }
+      total_pages: countTotalPages(total, size),
+    },
+  };
 }
